1ca1e3adfe5fd8049751d0631c34a767f586760f
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-04-15 11:41:00 +0300

    Поиск по хештегам

--- api/core/views.py
+++ api/core/views.py
@@ -3,6 +3,9 @@ from .serializers import PostSerializer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import pagination
+from rest_framework import generics
+from taggit.models import Tag
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,4 +14,28 @@ class PostViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
 
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
 
